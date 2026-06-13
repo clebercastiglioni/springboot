1782e9a716fde0e2a65b814fc72316bb762ecd3b
CLEBER RODRIGUES CASTIGLIONI <notri_damme@yahoo.com.br>
2026-06-13 13:47:32 -0300

    ajustes gerais

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+// HelloController feito em aula.
 @RestController
 public class HelloController {
 
